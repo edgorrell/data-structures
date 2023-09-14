595e043ca5bf661d94065a9a6cdd50eb274b1f8b
Evan Gorrell <edgorrell@stu.naperville203.org>
2023-09-14 13:29:53 -0500

    linked list demo

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -1,14 +1,20 @@
 import java.util.LinkedList;
 import java.util.ListIterator;
 
-/**
- * This program demonstrates the LinkedList class
- * and ListIterator class.
-*/
-public class ListDemo
-{
-    public static void main(String[] args)
-    {
-        
+public class ListDemo{
+    public static void main(String[] args){
+        LinkedList<String> list = new LinkedList<String>();
+        list.add("one"); list.add("two"); list.add("three"); list.add("four");
+        ListIterator<String> it = list.listIterator();
+
+        while(it.hasNext()){
+            it.next();
+            if(Math.random() < 0.5){
+                it.remove();
+            }
+        }
+        while(it.hasPrevious()){
+            System.out.println(it.previous());
+        }
     }
 }
