0050ae16c27bbbebe9f9b9dc8639e8646f9c2131
Evan Gorrell <edgorrell@stu.naperville203.org>
2023-09-25 13:18:48 -0500

    started sudoku, finished constructor and helper methods

--- Chapter 15 Activities/Sudoku/src/SudokuSolver.java
+++ Chapter 15 Activities/Sudoku/src/SudokuSolver.java
@@ -2,128 +2,128 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class SudokuSolver {
-    private final int M = 3;
-    private final int N = M * M;
+public class SudokuSolver{
+    // i guess assuming square grid
+    private final int squareSize; // length of each square
+    private final int size; // length of entire grid
+    private final int area; // area of grid, idk if helpful
     private int[][] grid;
     private ArrayList<Set<Integer>> rows;
     private ArrayList<Set<Integer>> cols;
     private ArrayList<Set<Integer>> squares;
     private Set<Integer> nums;
 
-    public SudokuSolver(String fileName) {
+    public SudokuSolver(String fileName, int size){
         // read the puzzle file
-        try (Scanner in = new Scanner(new File(fileName))) {
+        Scanner scan;
+        String str;
+        this.size = size;
+        this.squareSize = (int)(Math.sqrt(size));
+        area = size*size;
 
-            this.grid = new int[N][N];
-
-            for (int row = 0; row < N; row++) {
-                String line = in.next();
-
-                for (int col = 0; col < N; col++) {
-                    String strVal = line.substring(col, col + 1);
+        try{
+            scan = new Scanner(new File(fileName));
+            scan = new Scanner(new File(fileName));
+            this.grid = new int[size][size];
+            for (int row = 0; row < size; row++) {
+                String line = scan.next();
+                for (int col = 0; col < size; col++) {
+                    str = line.substring(col, col + 1);
                     int number;
-                    if (strVal.equals("x")) {
-                        number = 0;
+                    if (str.equals("x")) {
+                        number = -1;
                     } else {
-                        number = Integer.parseInt(strVal);
+                        number = Integer.parseInt(str);   
                     }
                     this.grid[row][col] = number;
                 }
             }
         } catch (FileNotFoundException e) {
             System.out.println("Cannot open: " + fileName);
+            System.exit(-1);
         }
 
+
+        Set<Integer> tempSet;
         // create the list of sets for each row (this.rows)
-        // ...
+        this.rows = new ArrayList<Set<Integer>>();
+        for(int i = 0; i < size; i++){
+            tempSet = new HashSet<Integer>();
+            for(int j = 0; j < size; j++){
+                tempSet.add(grid[i][j]);
+            }
+            this.rows.add(tempSet);
+        }
 
         // create the list of sets for each col (this.cols)
-        // ...
+        this.cols = new ArrayList<Set<Integer>>();
+        for(int i = 0; i < size; i++){
+            tempSet = new HashSet<Integer>();
+            for(int j = 0; j < size; j++){
+                tempSet.add(grid[j][i]);
+            }
+            this.cols.add(tempSet);
+        }
 
         // create the list of sets for each square (this.squares)
-        /* the squares are added to the list row-by-row:
-            0 1 2
-            3 4 5
-            6 7 8
-         */
-        // ...
+        this.squares = new ArrayList<Set<Integer>>();
+        int[] offsetX = {0,1,2,0,1,2,0,1,2};
+        int[] offsetY = {0,0,0,1,1,1,2,2,2};
+        for(int i = 0; i < size; i++){
+            tempSet = new HashSet<Integer>();
+            for(int j = 0; j < size; j++){
+                tempSet.add(this.grid[squareSize*offsetY[j]+offsetY[j]][squareSize*offsetX[j]+offsetX[j]]);
+            }
+            this.squares.add(tempSet);
+        }
 
         // create a hash set for [1..9] (this.nums)
-        // ...
-
-        // visually inspect that all the sets are correct
-        for (int row = 0; row < N; row++) {
-            System.out.println("row " + row + ": " + this.rows.get(row));
+        this.nums = new HashSet<Integer>();
+        for(int i = 0; i < size; i++){
+            this.nums.add(i);
         }
-        for (int col = 0; col < N; col++) {
-            System.out.println("col " + col + ": " + this.cols.get(col));
-        }
-        for (int square = 0; square < N; square++) {
-            System.out.println("square " + square + ": " + this.squares.get(square));
-        }
-        System.out.println(this.nums);
     }
 
-    public boolean solve() {
-        // find an empty location, if any
-        boolean finished = true;
+    public boolean solve(){
+        if(finished()){
+            return true;
+        }
+    }
+
+    public int[] nextEmpty(){
         int nextRow = -1;
         int nextCol = -1;
-        for (int row = 0; row < N && finished; row++) {
-            for (int col = 0; col < N && finished; col++) {
-                if (this.grid[row][col] == 0) {
-                    finished = false;
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if (this.grid[row][col] == -1) {
                     nextRow = row;
                     nextCol = col;
                 }
             }
         }
-
-        // the board is complete; we solved it
-        if (finished) {
-            return true;
+        if(nextRow == -1 || nextCol == -1){
+            return null;
         }
+        return new int[] {nextRow, nextCol};
+    }
 
-        // get all possible numbers for the row and column we are trying to populate
-        /*
-            Create a new set based on the this.nums and remove all elements in the sets
-            corresponding to nextRow, nextCol, and the corresponding square (use the
-            removeAll method).
-
-            Properly indexing the squares list of sets is tricky. Verify that your
-            algorithm is correct.
-         */
-        Set<Integer> possibleNums = new HashSet<Integer>();
-        possibleNums.addAll(this.nums);
-        
-        // ...
-
-        // if there are no possible numbers, we cannot solve the board in its current state
-        if (possibleNums.isEmpty()) {
-            return false;
-        }
-
-        // try each possible number
-        for (Integer possibleNum : possibleNums) {
-            // update the grid and all three corresponding sets with possibleNum
-            // ...
-
-            // recursively solve the board
-            if (this.solve()) {
-                // the board is solved!
-                return true;
-            } else {
-                /*
-                 Undo the move before trying another possible number by setting the corresponding
-                 element in the grid back to 0 and removing possibleNum from all three corresponding
-                 sets.
-                 */
-                // ...
+    public boolean finished(){
+        for(Set<Integer> row : this.rows){
+            if(!row.equals(this.nums)){
+                return false;
             }
         }
-
-        return false;
+        for(Set<Integer> col : this.cols){
+            if(!col.equals(this.nums)){
+                return false;
+            }
+        }
+        for(Set<Integer> square : this.squares){
+            if(!square.equals(this.nums)){
+                return false;
+            }
+        }
+        return true;
     }
 
     public String toString() {
@@ -141,15 +141,15 @@ public class SudokuSolver {
     }
 
     public static void main(String[] args) {
-        String fileName = "src/puzzle1.txt";
+        String fileName = "Chapter 15 Activities/Sudoku/src/puzzle1.txt";
 
-        SudokuSolver solver = new SudokuSolver(fileName);
+        SudokuSolver solver = new SudokuSolver(fileName,9);
         System.out.println(solver);
-        if (solver.solve()) {
-            System.out.println("Solved!");
-            System.out.println(solver);
-        } else {
-            System.out.println("Unsolveable...");
-        }
+        //if (solver.solve()) {
+        //    System.out.println("Solved!");
+        //    System.out.println(solver);
+        //} else {
+        //System.out.println("Unsolveable...");
+        //}
     }
 }
